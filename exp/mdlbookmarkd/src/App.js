f35686f7bc806537932f687564d562c3758054a9
Ulderico Cirello <u@cirello.eu>
2019-05-14 03:54:35 +0200

    exp/mdlbookmarkd: use drawer to manage browsing options

--- exp/mdlbookmarkd/src/App.js
+++ exp/mdlbookmarkd/src/App.js
@@ -3,34 +3,33 @@ import MaterialIcon from '@material/react-material-icon';
 import TopAppBar, { TopAppBarFixedAdjust, TopAppBarIcon, TopAppBarRow, TopAppBarSection, TopAppBarTitle } from '@material/react-top-app-bar';
 import Card, {
   CardPrimaryContent,
-  CardMedia,
   CardActions,
   CardActionButtons,
   CardActionIcons
 } from "@material/react-card";
 import Button from '@material/react-button';
 import { Headline6, Caption } from '@material/react-typography';
-import IconButton from '@material/react-icon-button';
 import { Cell, Grid, Row } from '@material/react-layout-grid';
-import Tab from '@material/react-tab';
-import TabBar from '@material/react-tab-bar';
+import Fab from '@material/react-fab';
+import Drawer, {
+  DrawerContent,
+  DrawerAppContent,
+} from '@material/react-drawer';
+import List, { ListItem, ListItemText, ListItemGraphic } from '@material/react-list';
 
 import './App.scss';
 import '@material/react-button/index.scss';
 import '@material/react-card/index.scss';
-import '@material/react-icon-button/index.scss';
+import '@material/react-drawer/index.scss';
+import '@material/react-fab/index.scss';
 import '@material/react-layout-grid/index.scss';
+import '@material/react-list/index.scss';
 import '@material/react-material-icon/index.scss';
-import '@material/react-tab-bar/index.scss';
-import '@material/react-tab-indicator/index.scss';
-import '@material/react-tab-scroller/index.scss';
-import '@material/react-tab/index.scss';
 import '@material/react-top-app-bar/index.scss';
 import '@material/react-typography/index.scss';
 
 class App extends React.Component {
-  state = { activeIndex: 0 };
-  handleActiveIndexUpdate = (activeIndex) => this.setState({ activeIndex });
+  state = { open: false };
 
   render() {
     const card = <Card style={{ margin: 5 }}>
@@ -57,75 +56,105 @@ class App extends React.Component {
       </CardActions>
     </Card>
 
+    const drawer = <Drawer dismissible open={this.state.open}>
+      <DrawerContent>
+        <List singleSelection selectedIndex={this.state.selectedIndex}>
+          <ListItem>
+            <ListItemGraphic graphic={<MaterialIcon icon='all_inbox' />} />
+            <ListItemText primaryText='Pending' />
+          </ListItem>
+          <ListItem>
+            <ListItemGraphic graphic={<MaterialIcon icon='bookmarks' />} />
+            <ListItemText primaryText='Bookmarks' />
+          </ListItem>
+          <ListItem>
+            <ListItemGraphic graphic={<MaterialIcon icon='compare_arrows' />} />
+            <ListItemText primaryText='Duplicated' />
+          </ListItem>
+        </List>
+      </DrawerContent>
+    </Drawer>
+
     return (
-      <div>
+      <div className='drawer-container'>
         <TopAppBar fixed>
           <TopAppBarRow>
             <TopAppBarSection align='start'>
               <TopAppBarIcon navIcon tabIndex={0}>
-                <MaterialIcon hasRipple icon='home' onClick={() => console.log('click')} />
+                <MaterialIcon hasRipple icon='menu' onClick={
+                  () => this.setState({ open: !this.state.open })
+                } />
               </TopAppBarIcon>
               <TopAppBarTitle>Bookmarks Manager</TopAppBarTitle>
             </TopAppBarSection>
             <TopAppBarSection align='end' role='toolbar'>
               <TopAppBarIcon actionItem tabIndex={0}>
                 <MaterialIcon
-                  aria-label="add new bookmark"
+                  aria-label="search"
                   hasRipple
-                  icon='add'
+                  icon='search'
                   onClick={() => console.log('print')}
                 />
               </TopAppBarIcon>
             </TopAppBarSection>
           </TopAppBarRow>
         </TopAppBar>
-        <TopAppBarFixedAdjust>
-          <TabBar
-            activeIndex={this.state.activeIndex}
-            handleActiveIndexUpdate={this.handleActiveIndexUpdate}
-          >
-            <Tab>
-              <span className='mdc-tab__text-label'>All</span>
-            </Tab>
-            <Tab>
-              <span className='mdc-tab__text-label'>Repeated</span>
-            </Tab>
-          </TabBar>
-
-          <Grid>
-            <Row>
-              <Cell columns={4}> {card} </Cell>
-              <Cell columns={4}> {card} </Cell>
-              <Cell columns={4}> {card} </Cell>
-            </Row>
-            <Row>
-              <Cell columns={4}> {card} </Cell>
-              <Cell columns={4}> {card} </Cell>
-              <Cell columns={4}> {card} </Cell>
-            </Row>
-            <Row>
-              <Cell columns={4}> {card} </Cell>
-              <Cell columns={4}> {card} </Cell>
-              <Cell columns={4}> {card} </Cell>
-            </Row>
-            <Row>
-              <Cell columns={4}> {card} </Cell>
-              <Cell columns={4}> {card} </Cell>
-              <Cell columns={4}> {card} </Cell>
-            </Row>
-            <Row>
-              <Cell columns={4}> {card} </Cell>
-              <Cell columns={4}> {card} </Cell>
-              <Cell columns={4}> {card} </Cell>
-            </Row>
-            <Row>
-              <Cell columns={4}> {card} </Cell>
-              <Cell columns={4}> {card} </Cell>
-              <Cell columns={4}> {card} </Cell>
-            </Row>
-          </Grid>
+        <TopAppBarFixedAdjust className='top-app-bar-fix-adjust'>
+          {drawer}
+          <DrawerAppContent className='drawer-app-content'>
+            <Grid>
+              <Row>
+                <Cell columns={4}> {card} </Cell>
+                <Cell columns={4}> {card} </Cell>
+                <Cell columns={4}> {card} </Cell>
+              </Row>
+              <Row>
+                <Cell columns={4}> {card} </Cell>
+                <Cell columns={4}> {card} </Cell>
+                <Cell columns={4}> {card} </Cell>
+              </Row>
+              <Row>
+                <Cell columns={4}> {card} </Cell>
+                <Cell columns={4}> {card} </Cell>
+                <Cell columns={4}> {card} </Cell>
+              </Row>
+              <Row>
+                <Cell columns={4}> {card} </Cell>
+                <Cell columns={4}> {card} </Cell>
+                <Cell columns={4}> {card} </Cell>
+              </Row>
+              <Row>
+                <Cell columns={4}> {card} </Cell>
+                <Cell columns={4}> {card} </Cell>
+                <Cell columns={4}> {card} </Cell>
+              </Row>
+              <Row>
+                <Cell columns={4}> {card} </Cell>
+                <Cell columns={4}> {card} </Cell>
+                <Cell columns={4}> {card} </Cell>
+              </Row>
+              <Row>
+                <Cell columns={4}> {card} </Cell>
+                <Cell columns={4}> {card} </Cell>
+                <Cell columns={4}> {card} </Cell>
+              </Row>
+              <Row>
+                <Cell columns={4}> {card} </Cell>
+                <Cell columns={4}> {card} </Cell>
+                <Cell columns={4}> {card} </Cell>
+              </Row>
+              <Row>
+                <Cell columns={4}> {card} </Cell>
+                <Cell columns={4}> {card} </Cell>
+                <Cell columns={4}> {card} </Cell>
+              </Row>
+            </Grid>
+          </DrawerAppContent>
         </TopAppBarFixedAdjust>
-      </div >
+        <Fab className='addNewBookmark' icon={
+          <MaterialIcon hasRipple icon='add' onClick={() => console.log('add')} />
+        } />
+      </div>
     );
   }
 }
