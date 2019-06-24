c65da499356c1f82c43024ec795f952960d0cd01
Ulderico Cirello <u@cirello.eu>
2019-06-24 19:35:20 +0200

    exp/rpi: first use of the button driver

--- exp/rpi/main.go
+++ exp/rpi/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"time"
 
 	"gobot.io/x/gobot"
@@ -9,13 +11,46 @@ import (
 )
 
 func main() {
+	button()
+}
+
+func button() {
+	r := raspi.NewAdaptor()
+
+	const redLedPort = "8"
+	if _, err := r.DigitalPin(redLedPort, "out"); err != nil {
+		log.Fatal("cannot reconfigure pin - redLed")
+	}
+	redLed := gpio.NewLedDriver(r, redLedPort)
+
+	const buttonPort = "12"
+	if _, err := r.DigitalPin(buttonPort, "in"); err != nil {
+		log.Fatal("cannot reconfigure pin - button")
+	}
+	button := gpio.NewButtonDriver(r, buttonPort)
+
+	work := func() {
+		button.On(gpio.ButtonPush, func(s interface{}) {
+			fmt.Println("button pushed")
+			redLed.Toggle()
+		})
+	}
+
+	robot := gobot.NewRobot("buttonBot",
+		[]gobot.Connection{r},
+		[]gobot.Device{button, redLed},
+		work,
+	)
+
+	robot.Start()
+}
+
+func blink() {
 	r := raspi.NewAdaptor()
 	yellowLed := gpio.NewLedDriver(r, "8")
 	redLed := gpio.NewLedDriver(r, "16")
 
 	work := func() {
-		yellowLed.On()
-		redLed.Off()
 		gobot.Every(200*time.Millisecond, func() {
 			yellowLed.Toggle()
 			redLed.Toggle()
