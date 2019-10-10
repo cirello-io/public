e13684c3cf2b11b2f7c6f03e6496b0f24c8df4a8
Ulderico Cirello <u@cirello.eu>
2019-10-09 18:40:48 -0700

    openapigen: first iteration of the swagger render tool

--- scripts/export-mirrors.sh
+++ scripts/export-mirrors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export REPOS="HumorChecker dynamolock errors goherokuname runner bookmarkd snippetsd cci mlflappygopher kohrah-ani oversight pglock junk svc gists exp poo alfredemoji pidctl"
+export REPOS="HumorChecker dynamolock errors goherokuname runner bookmarkd snippetsd cci mlflappygopher kohrah-ani oversight pglock junk svc gists exp poo alfredemoji pidctl openapigen"
 
 for repo in $REPOS
 do
