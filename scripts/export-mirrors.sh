04c14b95a2b1cde81e90eb6ab2e2583bdc66d3c7
Ulderico Cirello <u@cirello.eu>
2019-11-13 19:34:44 -0800

    scripts: retire errors

--- scripts/export-mirrors.sh
+++ scripts/export-mirrors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export REPOS="dynamolock runner bookmarkd oversight pglock pgqueue HumorChecker errors goherokuname snippetsd cci mlflappygopher kohrah-ani svc gists exp poo alfredemoji pidctl openapigen"
+export REPOS="dynamolock runner bookmarkd oversight pglock pgqueue HumorChecker goherokuname snippetsd cci mlflappygopher kohrah-ani svc gists exp poo alfredemoji pidctl openapigen"
 
 for repo in $REPOS
 do
